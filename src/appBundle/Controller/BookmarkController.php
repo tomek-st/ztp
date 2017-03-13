c507f37184f91d5f3bdf966976657357e38a310d
tomek-st <t.stepien@orange.pl>
2017-03-13 22:36:50 +0100

    assety i moduły pod paginację

--- src/appBundle/Controller/BookmarkController.php
+++ src/appBundle/Controller/BookmarkController.php
@@ -8,13 +8,22 @@ use appBundle\Repository\BookmarkRepository;
 
 class BookmarkController extends Controller
 {
-
+    /*
     public function indexAction()
     {
         $bookmarkRepository = new BookmarkRepository();
         $bookmarks = $bookmarkRepository->findAll();
         
         return $this->render('appBundle:bookmark:index.html.twig',['bookmarks' => $bookmarks]);
+    }*/
+    public function indexAction()
+    {
+        $bookmarks = $this->get('app.repository.bookmark')->findAll();
+
+        return $this->render(
+            'bookmark\bookmark.html.twig',
+            ['bookmarks' => $bookmarks]
+        );
     }
     public function detalAction($id)
     {
